520ad3aaf6b1ea92238ce8654d046bf0c08870e4
Kawish Gajadien <kawishg@gmail.com>
2020-10-30 10:06:26 +0100

    Added ft_memcpy

---
+++ ft_memcpy/ft_memcpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
+void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)dst)[i] = ((const char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
