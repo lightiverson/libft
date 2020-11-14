33628b517e10842f6171b8fb4bde61ea705c8981
Kawish Gajadien <kgajadie@f0r1s2.codam.nl>
2020-11-14 15:46:21 +0100

    Major work in progress on ft_split

--- ft_split.c
+++ ft_split.c
@@ -18,6 +18,13 @@ int ft_aantal_delims(char *s, char c)
 
 int *ft_get_indexes(char *s, char c)
 {
+	// nu check je waar de locatie van de delim is. je moet checken wat de locaties zijn van punten waar de delim niet is/ophoud te zijn. nested while loop, eerste tot 0, tweede tot niet equal met delim en null?
+	// 
+	// 
+	// 
+	// 
+	// 
+	// 
 	int* indexes;
 	int i;	
 	int j;
@@ -47,55 +54,63 @@ int *ft_get_indexes(char *s, char c)
 	return (indexes);
 }
 
-int ft_split(char *s, char c) // not the right prototype!!
+char **ft_split(char *s, char c) // not the right prototype!!
 {
 	int i;
+	int j;
 	int *indexes;
 	int numb_of_delims;
+	char** answer;
 
 	i = 0;
+	j = 0;
 	indexes = ft_get_indexes(s, c);
 	numb_of_delims = ft_aantal_delims(s, c);
-	printf("\nnumb_of_delims = %d\n", numb_of_delims);
+	// printf("\nnumb_of_delims = %d\n", numb_of_delims);
+
+	answer = malloc((numb_of_delims - 1) * sizeof(*answer) + 1);
+	if (!answer)
+		return (0);
 
 	while (i < numb_of_delims - 1)
 	{
-		printf("\nft_substr() = %s\n", ft_substr(s, indexes[i], indexes[i + 1] - 1 - indexes[i]));
+		// printf("\nft_substr() = %s\n", ft_substr(s, indexes[i], indexes[i + 1] - 1 - indexes[i]));
+		answer[j] = ft_substr(s, indexes[i], indexes[i + 1] - 1 - indexes[i]);
+		j++;
 		i++;
 	}
-
-	return (0);
+	return (answer);
 }
 
 
 
-int main (void)
-{
-	char *s = "Foo_Bar_Baz";
-	char c = '_';
-	// int y = ft_aantal_delims(s, c);
-	// printf("\ny = %d", y);
+// int main (void)
+// {
+// 	char *s = "Foo_Bar_Baz";
+// 	char c = '_';
+// 	// int y = ft_aantal_delims(s, c);
+// 	// printf("\ny = %d", y);
 	
-	// int *y = ft_get_indexes(s, c);
-	// int z = ft_aantal_delims(s, c);
-	// printf("\nz = %d\n", z);
+// 	// int *y = ft_get_indexes(s, c);
+// 	// int z = ft_aantal_delims(s, c);
+// 	// printf("\nz = %d\n", z);
 	
-	// int loop;
-	// for(loop = 0; loop < z; loop++)
-    // 	printf("%d ", y[loop]);
+// 	// int loop;
+// 	// for(loop = 0; loop < z; loop++)
+//     // 	printf("%d ", y[loop]);
 
-	// printf("\nft_substr() = %s\n", ft_substr(s, 0, 3));
-	// printf("\nft_substr() = %s\n", ft_substr(s, 4, 7 - 4));
-	// printf("\nft_substr() = %s\n", ft_substr(s, 8, 10 - 7));
+// 	// printf("\nft_substr() = %s\n", ft_substr(s, 0, 3));
+// 	// printf("\nft_substr() = %s\n", ft_substr(s, 4, 7 - 4));
+// 	// printf("\nft_substr() = %s\n", ft_substr(s, 8, 10 - 7));
 
-	// int i = 0;
-	// while (i < z)
-	// {
-	// 	printf("\n%s\n", ft_substr(s, y[i], y[i + 1] - 1 - y[i]));
-	// 	i++;
-	// }
+// 	// int i = 0;
+// 	// while (i < z)
+// 	// {
+// 	// 	printf("\n%s\n", ft_substr(s, y[i], y[i + 1] - 1 - y[i]));
+// 	// 	i++;
+// 	// }
 
-	ft_split(s, c);
+// 	ft_split(s, c);
 	
-	return (0);
-}
+// 	return (0);
+// }
