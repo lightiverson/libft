c8196ccaeb7c609aaeced77fa8e432adcf29d379
Kawish Gajadien <kgajadie@f0r1s2.codam.nl>
2020-11-14 15:32:02 +0100

    ft_split checkpoint wip

--- ft_split.c
+++ ft_split.c
@@ -27,6 +27,8 @@ int *ft_get_indexes(char *s, char c)
 	j = 0;
 	count = ft_aantal_delims(s, c);
 	indexes = malloc(count * sizeof(*indexes));
+	if (!indexes)
+		return (0);
 	
 	indexes[j] = 0;
 	j++;
@@ -45,6 +47,26 @@ int *ft_get_indexes(char *s, char c)
 	return (indexes);
 }
 
+int ft_split(char *s, char c) // not the right prototype!!
+{
+	int i;
+	int *indexes;
+	int numb_of_delims;
+
+	i = 0;
+	indexes = ft_get_indexes(s, c);
+	numb_of_delims = ft_aantal_delims(s, c);
+	printf("\nnumb_of_delims = %d\n", numb_of_delims);
+
+	while (i < numb_of_delims - 1)
+	{
+		printf("\nft_substr() = %s\n", ft_substr(s, indexes[i], indexes[i + 1] - 1 - indexes[i]));
+		i++;
+	}
+
+	return (0);
+}
+
 
 
 int main (void)
@@ -54,15 +76,26 @@ int main (void)
 	// int y = ft_aantal_delims(s, c);
 	// printf("\ny = %d", y);
 	
-	int *y = ft_get_indexes(s, c);
-	int z = ft_aantal_delims(s, c);
-	printf("\nz = %d\n", z);
-	int loop;
-	for(loop = 0; loop < z; loop++)
-    	printf("%d ", y[loop]);
+	// int *y = ft_get_indexes(s, c);
+	// int z = ft_aantal_delims(s, c);
+	// printf("\nz = %d\n", z);
+	
+	// int loop;
+	// for(loop = 0; loop < z; loop++)
+    // 	printf("%d ", y[loop]);
 
 	// printf("\nft_substr() = %s\n", ft_substr(s, 0, 3));
 	// printf("\nft_substr() = %s\n", ft_substr(s, 4, 7 - 4));
 	// printf("\nft_substr() = %s\n", ft_substr(s, 8, 10 - 7));
+
+	// int i = 0;
+	// while (i < z)
+	// {
+	// 	printf("\n%s\n", ft_substr(s, y[i], y[i + 1] - 1 - y[i]));
+	// 	i++;
+	// }
+
+	ft_split(s, c);
+	
 	return (0);
 }
