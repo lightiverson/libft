40ee963b80ba151766758020cef9ce9369ea2977
Kawish Gajadien <kawishg@gmail.com>
2020-11-16 14:12:15 +0100

    Norminetted ft_split.c not fully proofed yet.

--- ft_split.c
+++ ft_split.c
@@ -1,108 +1,89 @@
 #include "libft.h"
 
-int ft_aantal_delims(char const *s, char c)
+int		ft_aantal_delims(char const *s, char c)
 {
 	int i;
 	int len;
 	int k;
 
 	i = 0;
-    len = 0;
-    k = 0;
-    while (s[i])
-    {
-        while ((s[i] != c) && (s[i] != '\0'))
-        {
-        len++;
-        i++;
-        }
-        if (len > 0)
-        {
+	len = 0;
+	k = 0;
+	while (s[i])
+	{
+		while ((s[i] != c) && (s[i] != '\0'))
+		{
+			len++;
+			i++;
+		}
+		if (len > 0)
+		{
 			k = k + 2;
-            len = 0;
-        }
-        else
-            i++;
-    }
+			len = 0;
+		}
+		else
+			i++;
+	}
 	return (k);
 }
 
-int *ft_get_indexes(char const *s, char c)
+int		*ft_get_indexes(char const *s, char c)
 {
-	int i;	
-	int j;
-	int count;
-	int* indexes;
-	int len;
+	int		i;
+	int		j;
+	int		len;
+	int		*indexes;
 
 	i = 0;
 	j = 0;
 	len = 0;
-	count = ft_aantal_delims(s, c);
-	indexes = malloc(count * sizeof(*indexes));
+	indexes = malloc(ft_aantal_delims(s, c) * sizeof(*indexes));
 	if (!indexes)
 		return (0);
-    while (s[i])
-    {
-        while ((s[i] != c) && (s[i] != '\0'))
-        {
-        len++;
-        i++;
-        }
-        if (len > 0)
-        {
-			indexes[j] = i - len;
-			j++;
-			indexes[j] = len;
-			j++;
-            len = 0;
-        }
-        else
-            i++;
-    }
+	while (s[i])
+	{
+		while ((s[i] != c) && (s[i] != '\0'))
+		{
+			len++;
+			i++;
+		}
+		if (len > 0)
+		{
+			indexes[j++] = i - len;
+			indexes[j++] = len;
+			len = 0;
+		}
+		else
+			i++;
+	}
 	return (indexes);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	int i;
-	int j;	
-	int *indexes;
-	int numb_of_delims;
-	char **a;
+	int			i;
+	int			j;
+	int			*indexes;
+	char		**a;
 
 	if (!s)
 		return (0);
-
 	i = 0;
 	j = 0;
 	indexes = ft_get_indexes(s, c);
-	numb_of_delims = ft_aantal_delims(s, c);
-	a = malloc((numb_of_delims / 2) * sizeof(*a) + 1);
+	a = malloc((ft_aantal_delims(s, c) / 2) * sizeof(*a) + 1);
 	if (!a)
 	{
 		free(indexes);
 		free(a);
 		return (0);
 	}
-	while (i < numb_of_delims)
+	while (i < ft_aantal_delims(s, c))
 	{
 		a[j] = ft_substr(s, indexes[i], indexes[i + 1]);
-		i = i + 2;
 		j++;
+		i = i + 2;
 	}
 	a[j] = NULL;
 	return (a);
-}
-
-int main(void)
-{
-	char *s = "      split       this for   me  !       ";
-	char **result = ft_split(s, ' ');
-	while (*result)
-	{
-		printf("%s\n", *result);
-		result++;
-	}
-	return (0);
 }
