67863c7c42fc51a864d718b615c86af1d8b52041
Kawish Gajadien <kgajadie@f0r6s2.codam.nl>
2020-11-17 15:52:21 +0100

    Norm proofed memchr memcmp.

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_memchr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/17 15:46:24 by kgajadie      #+#    #+#                 */
+/*   Updated: 2020/11/17 15:47:23 by kgajadie      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned int i;
+	size_t i;
 
 	i = 0;
 	while (i < n)
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,11 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_memcmp.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/17 15:48:26 by kgajadie      #+#    #+#                 */
+/*   Updated: 2020/11/17 15:51:36 by kgajadie      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned long i = 0;
-	unsigned const char* str1 = s1;
-	unsigned const char* str2 = s2;
+	size_t					i;
+	unsigned const char		*str1;
+	unsigned const char		*str2;
 
+	i = 0;
+	str1 = s1;
+	str2 = s2;
 	while (i < n)
 	{
 		if (str1[i] != str2[i])
@@ -16,20 +31,3 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-
-// int main (void)
-// {
-// 	char s1[] = "\200ello, World!";
-// 	char s2[] = "\0Foo, barrrrrr";
-
-// 	int x = memcmp(s1, s2, 14);
-// 	printf("x = %d\n", x);
-
-// 	// printf("\n%d", 'H' - 'F');
-// 	// unsigned char y = '\200' - '\0';
-// 	// printf("\n%d", y);
-
-// 	int z = ft_memcmp(s1, s2, 14);
-// 	printf("z = %d\n", z);
-// 	return (0);
-// }
