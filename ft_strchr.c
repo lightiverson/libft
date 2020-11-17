eeaf986b6f161fb2d21b81a8b06be73791af98a8
Kawish Gajadien <kgajadie@f0r6s2.codam.nl>
2020-11-17 16:10:19 +0100

    Norm proofed ft_strchr.c. And edited the code.

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,36 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strchr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/17 16:08:08 by kgajadie      #+#    #+#                 */
+/*   Updated: 2020/11/17 16:09:04 by kgajadie      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-/*
 char	*ft_strchr(const char *s, int c)
 {
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == (char)c)
-		{
-			return (((char*)&(s[i])));
-		}
-		i++;
-	}
-	return (0);
-}
-*/
-
-char	*ft_strchr(const char *s, int c)
-{
-	// while (*s != (char)c)
-	// 	if (!*s++)
-	// 		return 0;
-	// return (char *)s;
-
-	int i = 0;
 	while (s[i] != (char)c)
 	{
 		if (s[i] == '\0')
 		{
-			return 0;
+			return (0);
 		}
 		i++;
 	}
